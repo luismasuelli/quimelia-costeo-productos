5cd20e2bbb3f77c9f42374a6d400c0a0e82abbc6
luismasuelli <luismasuelli@hotmail.com>
2016-10-15 16:45:25 -0500

    Client account form - only with service area

--- contacts/forms.py
+++ contacts/forms.py
@@ -6,3 +6,4 @@ from .models import Entity, ClientAccount
 EntityForm = modelform_factory(Entity, fields=('identification', 'identification_country', 'name', 'address', 'city',
                                                'provider'),
                                widgets={'provider': CheckboxInput(attrs={'data-toggle': 'toggle'})})
+ClientAccountForm = modelform_factory(ClientAccount, fields=('service_area',))
