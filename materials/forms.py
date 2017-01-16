ee90bb75bfd2f029756bbfc1d05db678cb0a0b7b
luismasuelli <luismasuelli@hotmail.com>
2017-01-15 22:27:29 -0500

    Agregando más campos a los forms

--- materials/forms.py
+++ materials/forms.py
@@ -3,9 +3,9 @@ from django.forms.widgets import CheckboxInput
 from .models import WorkForce, RawMaterial, Packaging
 
 
-PackagingForm = modelform_factory(Packaging, fields=('name', 'code', 'enabled', 'notes'),
+PackagingForm = modelform_factory(Packaging, fields=('name', 'code', 'price', 'enabled', 'notes'),
                                   widgets={'enabled': CheckboxInput(attrs={'data-toggle': 'toggle'})})
-RawMaterialForm = modelform_factory(RawMaterial, fields=('name', 'code', 'enabled', 'notes'),
+RawMaterialForm = modelform_factory(RawMaterial, fields=('name', 'code', 'kg_price', 'special', 'enabled', 'notes'),
                                     widgets={'enabled': CheckboxInput(attrs={'data-toggle': 'toggle'})})
-WorkForceForm = modelform_factory(WorkForce, fields=('name', 'code', 'enabled', 'notes'),
+WorkForceForm = modelform_factory(WorkForce, fields=('name', 'code', 'hourly_price', 'enabled', 'notes'),
                                   widgets={'enabled': CheckboxInput(attrs={'data-toggle': 'toggle'})})
